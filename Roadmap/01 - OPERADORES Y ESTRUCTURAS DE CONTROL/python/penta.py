e8dff1dedff12ac997d99714f0a067c0d37fa1c1
pentaaaaaaaaaa <pentamis2.0@gmail.com>
2024-08-08 19:58:51 -0500

    pentaaaa.py

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/penta.py
@@ -0,0 +1,22 @@
+#pythpn.org
+
+"""
+con triple comillas dobles se puede generar
+un comentario entre lineas y con un # se hace un
+comentario de una linea
+"""
+
+my_variable = "mi variable"
+my_variable = "el valor nuevo de mi variable"
+
+MY_CONSTANT = "mi constante"
+
+#en python como tal no existe codigo para generar una constante
+
+my int = 3
+my float = 2.3
+my bool = True                            #estos son los principios basicos de python
+my bool = False
+my string = "mi cadena de texto"
+
+string ("hola, python")
